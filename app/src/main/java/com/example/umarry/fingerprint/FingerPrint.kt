6509aa8d90de32d840d3680360eee49f6ddfbe46
HJ <gksgpwls0306@naver.com>
2023-07-12 19:28:03 +0900

    home 화면 구현 (슬라이드) 및 chatting 일부

--- app/src/main/java/com/example/umarry/fingerprint/FingerPrint.kt
+++
@@ -1,5 +0,0 @@
-package com.example.umarry.fingerprint
-
-class FingerPrint {
-
-}
---
+++ app/src/main/java/com/example/umarry/recycler/RecyclerView02Adapter.kt
@@ -0,0 +1,55 @@
+package com.example.umarry.recycler
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintSet.Layout
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.umarry.R
+import com.example.umarry.auth.UserDataModel
+import com.example.umarry.utils.FirebaseRef
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+
+class RecyclerView02Adapter(val context:Context, val items: MutableList<UserDataModel>):
+    RecyclerView.Adapter<RecyclerView02Adapter.ViewHolder>(){
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RecyclerView02Adapter.ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view :View = inflater.inflate(R.layout.itemrcv02,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView02Adapter.ViewHolder, position: Int) {
+        holder.binding(items[position])
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val image = itemView.findViewById<ImageView>(R.id.rcv02_img)
+        val nickname = itemView.findViewById<TextView>(R.id.rcv02_nickname)
+
+        fun binding(data: UserDataModel){
+            val storageRef = Firebase.storage.reference.child(data.uid!!).child("02.png")
+            storageRef.downloadUrl.addOnCompleteListener(OnCompleteListener { task ->
+                if(task.isSuccessful){
+                    Glide.with(context)
+                        .load(task.result)
+                        .into(image)
+                }
+            })
+            nickname.text = data.nickname
+        }
+    }
+}
